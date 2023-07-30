017a2cafae44baac9a5c66446143981f571cae74
Yousef Altaher <yousef.a.altaher@gmail.com>
2023-07-30 16:47:26 +0100

    finalize the emotion detector part

--- src/main.py
+++ src/main.py
@@ -8,15 +8,37 @@ import mediapipe as mp
 from fer import FER
 from engagementScore.srv import engagementScore, engagementScoreRequest, engagementScoreResponse, imgLstConverter, imgLstConverterRequest
 from PIL import Image as PILImage
+from cv_bridge3 import CvBridge
 
 def getData():
     # get image 
     img_msg = rospy.wait_for_message('/xtion/rgb/image_raw', Image)
 
     # get laser reading
-    laser_scan = rospy.wait_for_message('/scan_raw', LaserScan)
+    laser_scan = ""#rospy.wait_for_message('/scan_raw', LaserScan)
 
     return img_msg, laser_scan
+
+
+# def yawPitchRoll(faceMesh):
+#     # yaw
+#     print(faceMesh)
+
+#     return 0, 0, 0
+
+
+def emotionScore(score):
+    a = {
+        "angry" : -5,
+        "disgust": -4,
+        "fear" : -3,
+        "happy" : 5,
+        "sad" : 1,
+        "surprise" : 4,
+        "neutral" : 2,
+    }
+
+    return a[score]
 
 def locateEngagedObjects(req: engagementScoreRequest):
 
@@ -42,84 +64,45 @@ def locateEngagedObjects(req: engagementScoreRequest):
 
     # calculate face/eyes direction -> img
 
-    rospy.wait_for_service('ImgLstConverter')
-    imgConverterRes = rospy.ServiceProxy('ImgLstConverter', imgLstConverter)(img)
-    image2D = np.array(imgConverterRes.data, dtype="uint8").reshape(imgConverterRes.dimensions) #bgr8
-    EncodedImage2D = cv2.cvtColor(image2D, cv2.COLOR_BGR2RGB)
+    image2D = CvBridge().imgmsg_to_cv2_np(img)
+    EncodedImage2D = cv2.cvtColor(image2D, cv2.COLOR_BGR2RGB) # no need since _pnp will do it, here because will unuse _pnp
     EncodedImage2D.flags.writeable = False
 
-    new_image = PILImage.fromarray(EncodedImage2D)
-    new_image.save(f'newMT.png')
+    # new_image = PILImage.fromarray(EncodedImage2D)
+    # new_image.save(f'newMTr.png')
 
     for i in scores:
         # detect face orientation
-        mp_face_mesh = mp.solutions.face_mesh
-        face_mesh = mp_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence=0.5)
-
         r = EncodedImage2D[max(0, scores[i]["xywh"][1]):max(0, scores[i]["xywh"][1])+scores[i]["xywh"][3]+1, max(0, scores[i]["xywh"][0]):max(0, scores[i]["xywh"][0])+scores[i]["xywh"][2]+1]
+        r = r.copy(order='c')
+        # new_image = PILImage.fromarray(r)
+        # new_image.save(f'ddq{i}.png')
 
-        new_image = PILImage.fromarray(r)
-        new_image.save(f'newMT{i}.png')
-        continue
-        results = face_mesh.process(r)
+        # BaseOptions = mp.tasks.BaseOptions
+        # FaceLandmarker = mp.tasks.vision.FaceLandmarker
+        # FaceLandmarkerOptions = mp.tasks.vision.FaceLandmarkerOptions
+        # VisionRunningMode = mp.tasks.vision.RunningMode
 
-
-        img_h, img_w, img_c = EncodedImage2D.shape
-        face_3d = []
-        face_2d = []
-
-        if results.multi_face_landmarks:
-            for face_landmarks in results.multi_face_landmarks:
-                for idx, lm in enumerate(face_landmarks.landmark):
-                    if idx == 33 or idx == 263 or idx == 1 or idx == 61 or idx == 291 or idx == 199:
-                        if idx == 1:
-                            nose_2d = (lm.x * img_w, lm.y * img_h)
-                            nose_3d = (lm.x * img_w, lm.y * img_h, lm.z * 8000)
-
-                        x, y = int(lm.x * img_w), int(lm.y * img_h)
-
-                        # Get the 2D Coordinates
-                        face_2d.append([x, y])
-
-                        # Get the 3D Coordinates
-                        face_3d.append([x, y, lm.z])       
-
-                    # Convert it to the NumPy array
-                    face_2d = np.array(face_2d, dtype=np.float64)
-
-                    # Convert it to the NumPy array
-                    face_3d = np.array(face_3d, dtype=np.float64)
+        # options = FaceLandmarkerOptions(
+        #     base_options=BaseOptions(model_asset_path=model_path),
+        #     running_mode=VisionRunningMode.IMAGE)
+            
+        # with FaceLandmarker.create_from_options(options) as landmarker:
+        #     face_landmarker_result = landmarker.detect(r)
+        #     yaw, pitch, roll = yawPitchRoll(face_landmarker_result)
 
 
-            # The camera matrix
-            focal_length = 1 * img_w
-            cam_matrix = np.array([ [focal_length, 0, img_h / 2],
-                                    [0, focal_length, img_w / 2],
-                                    [0, 0, 1]]) 
-            # The Distance Matrix
-            dist_matrix = np.zeros((4, 1), dtype=np.float64)
-            # Solve PnP
-            success, rot_vec, trans_vec = cv2.solvePnP(face_3d, face_2d, cam_matrix, dist_matrix)
-            # Get rotational matrix
-            rmat, jac = cv2.Rodrigues(rot_vec)
-            # Get angles
-            angles, mtxR, mtxQ, Qx, Qy, Qz = cv2.RQDecomp3x3(rmat)
-            x = angles[0] * 360
-            y = angles[1] * 360
+        # emotions -> img
 
-            print(x)
-            print(y)
+        detector = FER()
+        emotion = detector.top_emotion(r)
+        scores[i]['score'] += emotionScore(emotion[0])
 
-
-
-            # emotions -> img
-
-            detector = FER()
-            emotion = detector.top_emotion(EncodedImage2D)
-
-            if emotion == "angry":
-                pass
-
+    print(scores[max(scores, key=lambda x: scores[x]['score'])])
+    print(scores[max(scores, key=lambda x: scores[x]['score'])]['xywh'])
+    res = engagementScoreResponse()
+    res.dimensions = list(scores[max(scores, key=lambda x: scores[x]['score'])]['xywh'])
+    return res
 
     # distraction detection -> img & laser
 
